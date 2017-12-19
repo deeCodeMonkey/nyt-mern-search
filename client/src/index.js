5f2c35acf9fd9998aef1c379a9b1bf3e3365b69c
deeCodeMonkey <deidra108@gmail.com>
2017-12-19 16:27:25 -0700

    add back registered svc worker

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Components/Container/Main';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Main />, document.getElementById('root'));
+registerServiceWorker();
 
 
 
